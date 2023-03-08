906cf1ade84abfea7662f508c4712eacd25e3beb
dbstarll <dbstarll@gmail.com>
2023-03-08 21:31:56 +0800

    测试获得OPEN_AI_KEY

--- src/test/java/io/github/dbstarll/utils/openai/OpenAiClientTest.java
+++ src/test/java/io/github/dbstarll/utils/openai/OpenAiClientTest.java
@@ -8,20 +8,18 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.ThrowingConsumer;
 
+import java.net.Proxy;
 import java.net.Proxy.Type;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class OpenAiClientTest {
+    private static final String TOKEN_KEY = "OPEN_AI_KEY";
+
     private void useClient(final ThrowingConsumer<OpenAiClient> consumer) throws Throwable {
-        System.out.println("Properties: ");
-        System.getProperties().entrySet().forEach(System.out::println);
-        System.out.println("Environments: ");
-        System.getenv().entrySet().forEach(System.out::println);
-        System.out.println("----------------------");
         try (CloseableHttpClient client = proxy(new HttpClientFactory()).setAutomaticRetries(false).build()) {
-            consumer.accept(new OpenAiClient(client, new ObjectMapper(), System.getProperty("OPEN_AI_KEY")));
+            consumer.accept(new OpenAiClient(client, new ObjectMapper(), getOpenAiKey()));
         }
     }
 
@@ -29,11 +27,29 @@ class OpenAiClientTest {
         final String socksProxyHost = System.getProperty("socksProxyHost");
         final String socksProxyPort = System.getProperty("socksProxyPort");
         if (StringUtils.isNoneBlank(socksProxyHost, socksProxyPort)) {
-            return factory.setProxy(HttpClientFactory.proxy(Type.SOCKS, socksProxyHost, Integer.parseInt(socksProxyPort)))
-                    .setResolveFromProxy(true);
+            final Proxy proxy = HttpClientFactory.proxy(Type.SOCKS, socksProxyHost, Integer.parseInt(socksProxyPort));
+            return factory.setProxy(proxy).setResolveFromProxy(true);
         } else {
             return factory;
         }
+    }
+
+    private String getOpenAiKey() {
+        final String keyFromProperty = System.getProperty(TOKEN_KEY);
+        if (StringUtils.isNotBlank(keyFromProperty)) {
+            return keyFromProperty;
+        }
+
+        final String opts = System.getenv("MAVEN_OPTS");
+        if (StringUtils.isNotBlank(opts)) {
+            for (String opt : StringUtils.split(opts)) {
+                if (opt.startsWith("-D" + TOKEN_KEY + "=")) {
+                    return opt.substring(3 + TOKEN_KEY.length());
+                }
+            }
+        }
+
+        return null;
     }
 
     @Test
